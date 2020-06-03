6ebd02aae85a63e1345f2cd5d10494d14318743b
kuldip11 <61648038+kuldip11@users.noreply.github.com>
2020-06-03 11:16:35 +0530

    Update MyArrayList.java

--- lists/MyArrayList.java
+++ lists/MyArrayList.java
@@ -8,18 +8,19 @@ public class MyArrayList {
 
 	public static void main(String[] args) {
 
-		List<String> fruits = new LinkedList();
+		List<String> fruits = new LinkedList(); //create an string type empty list 'fruits'
 		
-		fruits.add("Apple");
-		fruits.add("Orange");
-		fruits.add("Hi");
+		fruits.add("Apple"); //add 'Apple' in the empty list 'fruits' 
+		fruits.add("Orange"); //add 'Orange' in the list
+		fruits.add("Hi"); //add 'Hi' in the list
 		
-		String temp[] = new String[fruits.size()];
+		String temp[] = new String[fruits.size()]; 
 		
-		fruits.toArray(temp);
+		fruits.toArray(temp); //convert the list into an array and store in temp
 		
-		for(String e: temp) {
-			System.out.println(e);
+		for(String e: temp) 
+		{
+			System.out.println(e); //print the array indicated by 'e'
 		}
 	}
 
